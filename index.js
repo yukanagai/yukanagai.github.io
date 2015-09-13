d595401f6c1977a7126174018833ff9a3a0aff76
Yuka Nagai <yukanagai@gmail.com>
2015-09-12 22:08:14 -0400

    changing bg

---
+++ index.js
@@ -0,0 +1,14 @@
+$function() {
+
+  var scroll_pos = 0;
+    $(document).scroll(function() {
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 210) {
+            $(".first").css('background-color', 'blue');
+        } else {
+            $("body").css('background-color', 'red');
+        }
+    });
+});
+
+}
