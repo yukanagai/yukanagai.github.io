3985d8fdfb25fafc5c1867974465e37451e5f954
Yuka Nagai <yukanagai@gmail.com>
2015-09-27 08:08:54 -0400

    adding project page

--- index.js
+++ index.js
@@ -16,34 +16,4 @@ $(function() {
 
     });
 
-
-
-// $(document).ready(function(){
-  /*--------------------
-    BACKGROUND STUFF
-  ----------------------*/
-    // changebackground();
-    // setTimeout(function(){
-    //   $("body").removeClass("noTransition");
-    //   $("fixedBg").removeClass("noTransition");
-    //   changebackground();
-    // }, 2000)
-    // setInterval(function(){
-    //   changebackground();
-    // }, 20000);
-
-
-    /*--------------------
-      LOOPING COLORS
-    ----------------------*/
-  //   loopColors();
-  //     setTimeout(function(){ // hack change bg since the beginning
-  //       loopColors();
-  //     }, 2000)
-  //     setInterval(function(){
-  //       loopColors();
-  //   }, 10000);
-  // });
-
-
 });
