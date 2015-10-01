2e5aab02b68880c37f1c94bcf792bfc58b4d5445
Yuka Nagai <yukanagai@gmail.com>
2015-10-01 09:55:41 -0400

    updating

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 $(function() {
+
 	// change BG color on click:
 	$('.nav a').click(function() {
 		var colors = ['#7AAEBA', '#E8D5A5', '#C7ABB6', '#CBCBCB'];
 		var randColor = colors[Math.floor(Math.random()*colors.length)];
 		$('.pageWrapper').css('background-color', randColor);
 	});
+
+	// open About page on click
+	$('#aboutMe').click(function(){
+		$(this).css({position:'fixed', display: block, height: '100%', width: '100%', background-color: 'blue', opacity: 0.8});
+	});
+
 });
--- projects/weathervain/main.js
+++ projects/weathervain/main.js
@@ -1,4 +1,5 @@
  $(function() {
+
    $('button').click(function(event) {
      event.preventDefault();
      var city = $(".pure-input-rounded").val();
@@ -17,9 +18,18 @@
        }
      });
 
+    var bgimgs = [
+      'https://images.unsplash.com/reserve/nLOrZieVR5OWjjIo11ZY_final11.jpg?q=80&fm=jpg&s=d167329f8b1c07812fea3500f84e4549', 
+      'https://images.unsplash.com/photo-1421081177127-339f586c9b49?q=80&fm=jpg&s=36a427925803fdc3ba3038b6134d6372', 
+      'https://unsplash.com/photos/gHtB1alGIWg/download'
+      ];
 
-   });
+    var randImg = bgimgs[Math.floor(Math.random()*bgimgs.length)];
+    $('body').css({'background-image' : "url("randImg")")};
+
+    });f
+});
 
 
 
- });
+ // });
