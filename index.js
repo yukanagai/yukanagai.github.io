555d8622f456eca30b77923a07fa933c2fb47b1b
Yuka Nagai <yukanagai@gmail.com>
2015-10-04 23:29:01 -0400

    updating homepage

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ $(function() {
 
 	// change BG color on click:
 	$('.nav a').click(function() {
-		var colors = ['#7AAEBA', '#E8D5A5', '#C7ABB6', '#70C1B3', '#DAFFEF', '#FFE5D9', '#FF9B71'];
+		var colors = ['#E8D5A5', '#C7ABB6', '#DAFFEF', '#FFE5D9', '#DDE6F2'];
 		var randColor = colors[Math.floor(Math.random()*colors.length)];
 		$('.pageWrapper').css('background-color', randColor);
 	});
+
+
 
 	// open About page on click
 	// $('#aboutMe').click(function(){
