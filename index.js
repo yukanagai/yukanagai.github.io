c8ee193c3139ad05a4b73284f379c8bd08c1053c
Yuka Nagai <yukanagai@gmail.com>
2015-10-05 22:40:57 -0400

    updated style

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 $(function() {
 
 	// change BG color on click:
-	$('.nav a').click(function() {
-		var colors = ['#E8D5A5', '#C7ABB6', '#DAFFEF', '#FFE5D9', '#DDE6F2'];
-		var randColor = colors[Math.floor(Math.random()*colors.length)];
-		$('.pageWrapper').css('background-color', randColor);
+	// $('.nav a').click(function() {
+	// 	var colors = ['#E8D5A5', '#C7ABB6', '#DAFFEF', '#FFE5D9', '#DDE6F2'];
+	// 	var randColor = colors[Math.floor(Math.random()*colors.length)];
+	// 	$('.pageWrapper').css('background-color', randColor);
+	// });
+
+	$('#palmleaf').click(function() {
+		$(this).addClass('animated tada').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+			function() {
+				$(this).removeClass('animated tada');
+			});
 	});
 
 });
