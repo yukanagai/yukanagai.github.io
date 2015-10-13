3acf7687449a8daca99c09f8ca9401f748ffe5c0
Yuka Nagai <yukanagai@gmail.com>
2015-10-12 22:11:21 -0400

    updated jquery scrolling

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 $(function() {
 
-	// change BG color on click:
-	// $('.nav a').click(function() {
-	// 	var colors = ['#E8D5A5', '#C7ABB6', '#DAFFEF', '#FFE5D9', '#DDE6F2'];
-	// 	var randColor = colors[Math.floor(Math.random()*colors.length)];
-	// 	$('.pageWrapper').css('background-color', randColor);
-	// });
-
-	$('.palmleaf').click(function() {
-		$(this).addClass('animated tada').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
-			function() {
-				$(this).removeClass('animated tada');
-			});
-	});
-
-	$(window).scroll(function(i){
-    	var scrollVar = $(window).scrollTop();
-    	$('.palmleaf').css({'top': .1*scrollVar });
-    	$('.palmleaf').css({'opacity':( 100-scrollVar )/100});
+	var $root = $('html, body');
+	$('a').click(function() {
+	    var href = $.attr(this, 'href');
+	    $root.animate({
+	        scrollTop: $(href).offset().top
+	    }, 1000, function () {
+	        window.location.hash = href;
+	    });
+	    return false;
 	});
 });
