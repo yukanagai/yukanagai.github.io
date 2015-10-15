52e43cb6efcb5d26bc58411b59157a86d40678de
Yuka Nagai <yukanagai@gmail.com>
2015-10-14 22:04:39 -0400

    added clouds

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ $(function() {
 	    });
 	    return false;
 	});
+
+	$(window).scroll(function(){
+    windowTop = $(window).scrollTop();
+    $('#cloud1').css({
+      'transform':'translateX('+(windowTop) * .3 +'px)'
+    });
+
+    $('#cloud2').css({
+      'transform':'translateX('+(windowTop) * .7 +'px)'
+    });
+  });
+
 });
