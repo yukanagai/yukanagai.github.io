c1fce2d7f159c90d60eca6b941631c91a0047c45
Yuka Nagai <yukanagai@gmail.com>
2016-12-30 22:05:09 -0500

    rebundling

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,7 +1,6 @@
 import '../css/master.scss';
-import PageContainer from './PageContainer';
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import PageContainer 	from './PageContainer';
+import React 			from 'react';
+import ReactDOM 		from 'react-dom';
 
 ReactDOM.render(<PageContainer />, document.getElementById('app'));
